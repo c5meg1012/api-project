b66801c72c0894d6d25180b6c0aea2efd86bf0b8
Megumi Mochizuki <mochizuki@rista.jp>
2017-12-16 13:21:08 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216035325) do
+ActiveRecord::Schema.define(version: 20171216041615) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
