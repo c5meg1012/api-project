edd979573e60214136ace57b754317269ef377de
Megumi Mochizuki <mochizuki@rista.jp>
2017-12-16 13:27:03 +0900

    permit user's name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.fetch(:user, {})
+      params.fetch(:user, {}).permit(:name)
     end
 end
