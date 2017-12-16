45f40dede3f87494f8c94e20ad6db3a9340861c4
Megumi Mochizuki <mochizuki@rista.jp>
2017-12-16 14:02:42 +0900

    config and update rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:name) { |n| "ユーザー#{n}" }
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
+    # Userを作成
+    before { @users = FactoryGirl.create_list(:user, 2) }
+    it "index" do
       get users_path
+
+      # ステータスコードのチェック
       expect(response).to have_http_status(200)
+
+      # JSONの内容のチェック
+      json = JSON.parse(response.body)
+      expect(json.size).to     eq @users.count
+      expect(json[0]["id"]).to eq @users[0].id
+      expect(json[1]["id"]).to eq @users[1].id
     end
   end
 end
